7793737c5c46fc792aab398a10a3880e7fe1e0d7
space-up <gourish0292.becse24@chitkara.edu.in>
2025-01-29 12:06:10 +0530

    Solution to Check Not Equal To

---
+++ Check Not Equal To@@67882a731b4392d8fc1577db/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    float a;
+    float b;
+    scanf("%f%f",&a,&b);
+    if(a!=b){
+        printf("True");
+    }
+    if(a==b){
+        printf("False");
+    }
+    return 0;
+}
