5b86e24831fc0a44dbaee2225754999b5ac3d303
space-up <gourish0292.becse24@chitkara.edu.in>
2025-01-31 20:27:01 +0530

    Solution to Logical NOT Operation

---
+++ Logical NOT Operation@@67882a731b4392d8fc1577de/code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int a;
+    scanf("%d",&a);
+    if (a>0) {
+        printf("True");
+
+    if (a<0) {
+        prinf("False");
+    }
+    }
+
+    
+    return 0;
+}
