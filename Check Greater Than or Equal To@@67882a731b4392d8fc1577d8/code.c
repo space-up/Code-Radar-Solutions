96f694716c1aeee801753ae19eb0c52c53718aa8
space-up <gourish0292.becse24@chitkara.edu.in>
2025-02-03 10:23:55 +0530

    Solution to Check Greater Than or Equal To

---
+++ Check Greater Than or Equal To@@67882a731b4392d8fc1577d8/code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()  {
+    float a;
+    scanf("%f",&a);
+    float b;
+    scanf("%f",&b);
+    if (a>=b)   {
+        printf("True");
+    }
+    else () {
+        prinf("False");
+    }
+    
+    return 0;
+}
